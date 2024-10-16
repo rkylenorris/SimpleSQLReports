cb0a77958427cabf13ceffd211586c86c1ee677a
R. Kyle Norris <r.kyle.norris@gmail.com>
2024-10-15 20:14:54 -0400

    added sql select class

--- Class/SimpleSQLReports.SqlQuery.ps1
+++ Class/SimpleSQLReports.SqlQuery.ps1
@@ -1,16 +1,17 @@
 
-class SqlQuery {
+class SqlSelectQuery {
 
     [string]$ConnectionString
-    [system.data.SqlClient.SQLConnection]$Connection
+    $Connection
     [string]$QueryText
     [string]$QueryPath
     [pscredential]$Credentials
     [System.Data.DataSet]$QueryResults
     [System.Data.SqlClient.SqlCredential]$sqlCredentials
 
-    SqlQuery([string]$connStr, [string]$query, [pscredential]$creds){
+    SqlSelectQuery([string]$connStr, [string]$query, [pscredential]$creds){
 
+        $this.ConnectionString = $connStr
         $this.Credentials = $creds
         $this.Credentials.Password.MakeReadOnly()
         $this.sqlCredentials = [System.Data.SqlClient.SqlCredential]::new($This.Credentials.UserName, $This.Credentials.Password)
@@ -24,8 +25,29 @@ class SqlQuery {
         }
 
         $this.Connection = new-object system.data.SqlClient.SQLConnection($this.ConnectionString)
-        $this.Connection.Credential = $this.sqlCredentials
+        # $this.Connection.Credential = $this.sqlCredentials
+        # $this.Connection.Open()
 
     }
 
+    [System.Data.DataSet] InvokeQuery(){
+        $sqlCmd = New-Object System.Data.SqlClient.SqlCommand($this.QueryText, $this.Connection)
+        $this.Connection.Open()
+        $sqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter $sqlCmd
+        $dataset = New-Object System.Data.DataSet
+        $sqlAdapter.Fill($dataset) | Out-Null
+        $this.Connection.Close()
+
+        return $dataset
+    }
+
 }
+
+$connStr = "Data Source=localhost\SQLEXPRESS;Initial Catalog=PoShTest;Integrated Security=SSPI;"
+$q = "SELECT * FROM [ForTesting]"
+$C = [pscredential]::new("rkynorris", ('Sup3rG4y!' | ConvertTo-SecureString -AsPlainText -Force))
+
+$sqlSelect = [SqlSelectQuery]::new($connStr, $q, $C)
+$data = $sqlSelect.InvokeQuery()
+
+
